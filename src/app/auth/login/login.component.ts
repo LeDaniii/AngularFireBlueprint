9a19d7b8dcace0e310d400e80903d4892a02890e
LeDaniii <danielmatzer@gmail.com>
2023-03-29 23:28:28 +0200

    update

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { LoginForm } from '../../interfaces/auth';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(private authService: AuthService) {}
 
+  form: LoginForm = {
+    email: '',
+    password: '',
+  };
+
+  message: string = '';
+  messageBackground: string = '';
+
+  submit() {
+    if (this.form.password === '' || this.form.email === '') {
+      this.messageBackground = 'red';
+      this.message = 'Fill in all fields';
+      return;
+    }
+
+    this.authService.login(this.form);
+  }
+
+  isLoading() {
+    return this.authService.isLoading;
+  }
 }
